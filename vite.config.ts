28e42aaa2edc553528c00ea99182ffd6a62cfe65
Dongri162 <domkingnguyen@gmail.com>
2026-02-25 01:42:27 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-    base: '/QLNN_TP3/',
+    base: '/vite.config.ts/',
       server: {
         port: 3000,
         host: '0.0.0.0',
